cfc3f9cb272a9ec4df9624e0b9db33a3ba958d4e
Louis Turpault <louis.turpault@gmail.com>
2026-06-22 11:49:13 -0400

    feat: added first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import subprocess
+from datetime import datetime
+
+
+def main():
+
+    get_update_of_service("nginx")
+
+
+def get_update_of_service(service: str):
+    unparsed_time = subprocess.run(
+        ["/usr/bin/systemctl", "show--property=ActiveEnterTimestamp", service],
+        capture_output=True,
+        check=True,
+    ).stdout.decode("utf-8")
+    list_of_time = unparsed_time.split("=")[-1]
+    datetime.strptime(list_of_time, "%c")
+
+
+if __name__ == "__main__":
+    main()

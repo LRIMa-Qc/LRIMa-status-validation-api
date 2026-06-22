ad892bbefe537c639f814ccb0c9cdca859030042
Louis Turpault <louis.turpault@gmail.com>
2026-06-22 12:07:42 -0400

    fix: formatting

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 
 def main():
-
     get_update_of_service("nginx")
 
 
@@ -14,7 +13,12 @@ def get_update_of_service(service: str):
         check=True,
     ).stdout.decode("utf-8")
     list_of_time = unparsed_time.split("=")[-1]
-    datetime.strptime(list_of_time, "%c")
+    # ActiveEnterTimestamp=Wed 2026-06-17 16:48:16 EDT
+    date = datetime.strptime(
+        list_of_time,
+        "%a %Y-%m-%d %H:%M:%S %Z",
+    )
+    print(date)
 
 
 if __name__ == "__main__":
